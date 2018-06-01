23669199933c753873ba812586984c23d0b89365
eduardoalexandre1000@gmail.com <edualecandre.f@gmail.com>
2018-06-01 04:10:37 -0300

    Cliente, TipoCliente, Telefones e Enderecoa

---
+++ src/main/java/com/edu/cursomc/repositories/EnderecoRepository.java
@@ -0,0 +1,11 @@
+package com.edu.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.edu.cursomc.domain.Endereco;
+
+@Repository
+public interface EnderecoRepository extends JpaRepository<Endereco, Integer> {
+
+}
