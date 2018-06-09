382de040cddbe544c90b65162cc64174af54c839
eduardoalexandre1000@gmail.com <eduardoalexandre1000@gmail.com>
2018-06-09 04:30:59 -0300

    implementacao de delecao cataegorias

--- src/main/java/com/edu/cursomc/services/CategoriaService.java
+++ src/main/java/com/edu/cursomc/services/CategoriaService.java
@@ -3,10 +3,12 @@ package com.edu.cursomc.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.edu.cursomc.domain.Categoria;
 import com.edu.cursomc.repositories.CategoriaRepository;
+import com.edu.cursomc.services.execeptions.DataIntegrityException;
 import com.edu.cursomc.services.execeptions.ObjectNotFoundException;
 
 @Service
@@ -29,5 +31,16 @@ public class CategoriaService {
 	public Categoria update(Categoria obj) {
 		find(obj.getId());
 		return categoriaRepository.save(obj);
+	}
+
+	public void delete(Integer id) {
+		find(id);
+		try {
+			categoriaRepository.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possivel excluir uma categoria que possui produtos!");
+		}
+		
+		
 	}
 }
